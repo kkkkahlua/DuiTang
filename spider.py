8bb41198fbcdc2c33ac677dcc31d8fd201f5f324
kahlua <fan_0117@sina.com>
2017-12-05 04:21:28 +0800

    fix the problem of directory structure (caused by not returning to the proper place after jumping out of the recursion)

--- spider.py
+++ spider.py
@@ -11,7 +11,14 @@ class Spider():
 	def __init__(self):
 		pass
 
-	def save_picture(self, src):
+	def save_picture(self, src, type, name):
+		if (type == 1):
+			shell.mkdir('album\\'+name)
+			shell.cd('album\\'+name)
+		else:
+			shell.mkdir('search\\'+name)
+			shell.cd('search\\'+name)
+
 		content = requests.get(src).text
 		pattern = re.compile('"photo".*?"path":"(.*?)"},"msg":"(.*?)"', re.S)
 		results = re.findall(pattern, content)
@@ -32,6 +39,40 @@ class Spider():
 			shell.save(strproc.del_spec(name),url)
 
 
+	def dfs_all(self, src, type, name):
+		#print(src)
+		if (type == 1):
+			self.save_picture(src, type, name)
+		elif (type == 2):
+			
+			content = requests.get(src).text
+			pat_alb = re.compile('"album":{"id":(.*?),"name":"(.*?)"')
+			results = re.findall(pat_alb, content)
+
+			for result in results:
+				print(result)
+				self.album_proc(result)
+
+			self.save_picture(src, type, name)
+
+		content = requests.get(src).text
+		pat_num = re.compile('}],"more":(.*?),"limit"')
+		s_num = re.search(pat_num, content)
+		if (s_num is None): return
+		num = int(s_num.group(1))
+		#print(num)
+		if (num != 0): self.dfs_all(strproc.next_url(src), type, name)
+
+
+	#	func:	save all pictures in the designated album
+	#	notice: albums do not contain urls that point to other websites,
+	#			as a result, dfs will end here
+	def dfs_album(self, src, name):
+		print(src)
+		self.dfs_all(src, 1, name)
+
+	def dfs_search(self, src, name):
+		self.dfs_all(src, 2, name)
 
 	def album_proc(self, result):
 		id,album = result
@@ -41,45 +82,7 @@ class Spider():
 		bloomfilter.insert(album, 1)
 
 		print(id,album)
-		shell.mkdir('album\\'+album)
-		shell.cd('album\\'+album)
-		self.dfs_album('https://www.duitang.com/napi/blog/list/by_album/?album_id='+id+'&limit=24&include_fields=top_comments%2Cis_root%2Csource_link%2Cbuyable%2Croot_id%2Cstatus%2Clike_count%2Csender%2Creply_count&start=24')
-		shell.cd('')
-
-	def dfs_all(self, src, type):
-		#print(src)
-		if (type == 1):
-			self.save_picture(src)
-		elif (type == 2):
-			'''
-			content = requests.get(src).text
-			pat_alb = re.compile('"album":{"id":(.*?),"name":"(.*?)"')
-			results = re.findall(pat_alb, content)
-
-			for result in results:
-				self.album_proc(result)
-			'''
-			self.save_picture(src)
-
-		content = requests.get(src).text
-		pat_num = re.compile('}],"more":(.*?),"limit"')
-		s_num = re.search(pat_num, content)
-		if (s_num is None): return
-		num = int(s_num.group(1))
-		#print(num)
-		if (num != 0): self.dfs_all(strproc.next_url(src), type)
-
-
-	#	func:	save all pictures in the designated album
-	#	notice: albums do not contain urls that point to other websites,
-	#			as a result, dfs will end here
-	def dfs_album(self, src):
-		print(src)
-		self.dfs_all(src, 1)
-
-	def dfs_search(self, src):
-		self.dfs_all(src, 2)
-
+		self.dfs_album('https://www.duitang.com/napi/blog/list/by_album/?album_id='+id+'&limit=24&include_fields=top_comments%2Cis_root%2Csource_link%2Cbuyable%2Croot_id%2Cstatus%2Clike_count%2Csender%2Creply_count&start=24', album)
 
 	def work(self, src):
 		content = requests.get(src).text
@@ -95,7 +98,7 @@ class Spider():
 
 		print('')
 
-		'''
+		
 		#	album
 		pattern2_1 = re.compile('href="/album/\?id=(.*?)".*?lstitle">(.*?)</span>', re.S)
 		results2_1 = re.findall(pattern2_1, content)		
@@ -107,7 +110,7 @@ class Spider():
 			self.album_proc(result)
 		
 		print('')
-		'''
+		
 
 		#	search
 		pattern3 = re.compile('<a target=".*?href=".*?search(.*?)">(.*?)</a>', re.S)
@@ -117,10 +120,7 @@ class Spider():
 			url, name = result
 			name = re.sub('\s', '', name)
 			print(url, name)
-			shell.mkdir('search\\'+name)
-			shell.cd('search\\'+name)
-			self.dfs_search('https://www.duitang.com/napi/blog/list/by_search/?kw='+urllib.parse.quote(name)+'&type=feed&include_fields=top_comments%2Cis_root%2Csource_link%2Citem%2Cbuyable%2Croot_id%2Cstatus%2Clike_count%2Csender%2Calbum&_type=&start=24')
-			shell.cd('')
+			self.dfs_search('https://www.duitang.com/napi/blog/list/by_search/?kw='+urllib.parse.quote(name)+'&type=feed&include_fields=top_comments%2Cis_root%2Csource_link%2Citem%2Cbuyable%2Croot_id%2Cstatus%2Clike_count%2Csender%2Calbum&_type=&start=24', name)
 	
 
 
